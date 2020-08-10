9c3cbb9541dee046fadbf68e588198bf5dde48bd
anb-roundsqr <adinarayana.bandari@roundsqr.net>
2020-08-10 19:28:06 +0530

    cors headers1

--- ChatBot/settings.py
+++ ChatBot/settings.py
@@ -108,6 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
@@ -127,7 +130,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-CORS_ORIGIN_ALLOW_ALL = True
-
-CORS_ALLOW_CREDENTIALS = True
